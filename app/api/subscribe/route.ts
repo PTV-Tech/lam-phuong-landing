d8a94166af93179399c0974ee52db6bccb58ce40
VinhPT2 <vinhpt2@vng.com.vn>
2025-03-20 17:46:58 +0700

    fix(route.ts): enhance error handling by including error message and keys in the response for better debugging information

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -12,9 +12,9 @@ export async function POST(request: Request) {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log("=>(route.ts:18) error", error);
-    return new Response(JSON.stringify({ ok: false, error: JSON.stringify(error) }), {
+    return new Response(JSON.stringify({ ok: false, error: error.message, keys: Object.keys(error) }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
