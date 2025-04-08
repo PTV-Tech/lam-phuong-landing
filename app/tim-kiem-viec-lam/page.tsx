620433f3be9e3e5814b26ca6d201fc20577c67b2
VinhPT2 <vinhpt2@vng.com.vn>
2025-04-09 00:34:25 +0700

    fix(route.ts): handle potential undefined records in responseLocation to prevent runtime errors feat(header): refactor onClick handler to use useCallback for better performance and readability fix(page.ts): increase PAGE_SIZE from 3 to 5 to display more job listings per page feat(page.ts): add statusFormula to filter for approved job listings in the query

--- app/tim-kiem-viec-lam/page.tsx
+++ app/tim-kiem-viec-lam/page.tsx
@@ -2,7 +2,7 @@ import ClientView from "./components/ClientView";
 import { GetRecordsResponse, JobFields, LocationFields } from "@/type";
 
 const BASE_URL = `https://api.airtable.com/v0/${process.env.BASE_ID}`;
-const PAGE_SIZE = 3;
+const PAGE_SIZE = 5;
 const fields =
   "fields%5B%5D=Ti%C3%AAu+%C4%91%E1%BB%81&fields%5B%5D=M%C3%B4+t%E1%BA%A3+c%C3%B4ng+vi%E1%BB%87c&fields%5B%5D=Khu+v%E1%BB%B1c&fields%5B%5D=Slug";
 
@@ -44,12 +44,14 @@ export default async function Page({
         (location) => `FIND("${location}", ARRAYJOIN({Khu vực}, ","))`,
       );
     }
+    const statusFormula = `FIND("Approved", {Status})`;
 
     const formula = [
       ...jobTypesFormula,
       ...jobCategoriesFormula,
       ...productGroupsFormula,
       ...locationsFormula,
+      statusFormula,
     ].filter((item) => !!item);
     if (formula.length > 0) {
       return `OR(${formula.join(", ")})`;
@@ -79,7 +81,7 @@ export default async function Page({
     },
   ).then((res) => res.json());
   let locationMap: { [key: string]: string } = {};
-  responseLocation.records.forEach((record: any) => {
+  (responseLocation.records || []).forEach((record: any) => {
     locationMap[record.id] = record.fields["Name"];
   });
 
